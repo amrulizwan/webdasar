6ce3088d6f0808b42e32a02bc29c32409cfde055
Amrul Izwan <amrulizwan7@gmail.com>
2023-10-19 08:14:45 +0800

    update index

---
+++ buku/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <center>
+        <h3>Halaman Utama</h3>
+    </center>
+    <ul>
+        <li><a href="penerbit/tampil.php">Halaman Penerbit</a></li>
+        <li><a href="penulis/tampil.php">Halaman Penulis</a></li>
+    </ul>
+</body>
+
+</html>
